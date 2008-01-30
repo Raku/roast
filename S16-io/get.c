8de0b6e1311a999e279fc9fac5f4131bb3de84b2
cosimo <cosimo@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-01-30 22:44:31 +0000

    [t/spec] Test suite refactoring. Starting work on S16! Moved io::dir and getc tests under S16-io and S16-filehandles
    git-svn-id: http://svn.pugscode.org/pugs@19763 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ S16-io/get.c
@@ -0,0 +1,30 @@
+use v6-alpha;
+use Test;
+plan 1;
+
+# L<S16/"Input and Output"/"getc">
+
+sub nonce () { return (".$*PID." ~ int rand 1000) }
+
+if $*OS eq "browser" {
+  skip_rest "Programs running in browsers don't have access to regular IO.";
+  exit;
+}
+
+my $tmpfile = "temp-test" ~ nonce();
+{
+  my $fh = open($tmpfile, :w) orelse die "Couldn't open \"$tmpfile\" for writing: $!\n";
+  print $fh: "TestÄÖÜ\n\n0";
+  close $fh orelse die "Couldn't close \"$tmpfile\": $!\n";
+}
+
+{
+  my $fh = open $tmpfile orelse die "Couldn't open \"$tmpfile\" for reading: $!\n";
+  my @chars;
+  push @chars, $_ while defined($_ = getc $fh);
+  close $fh orelse die "Couldn't close \"$tmpfile\": $!\n";
+
+  is ~@chars, "T e s t Ä Ö Ü \n \n 0", "getc() works even for utf-8 input";
+}
+
+END { unlink $tmpfile }
