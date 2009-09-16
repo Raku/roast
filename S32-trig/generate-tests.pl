63273a504a010c6003923862d6ecc82e51fea122
colomon <colomon@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-09-16 13:25:23 +0000

    [t/spec] Replace the ugly code as qq strings (which required all sorts of nasty escaping) with q strings plus simple hand interpolation using subst.
    git-svn-id: http://svn.pugscode.org/pugs@28251 c213334d-75ef-0310-aa23-eaa082d1ae64

--- S32-trig/generate-tests.pl
+++ S32-trig/generate-tests.pl
@@ -21,129 +21,137 @@ class TrigFunction
                    :$angle_and_results_name, 
                    :$setup_block);
     }
-    
-    
+        
     method dump_forward_tests($file) {
-        my $code = qq[
-            # {$.function_name} tests
+        my $code = q[
+            # $.function_name tests
 
-            for $.angle_and_results_name -> \$angle
-            \{
+            for $.angle_and_results_name -> $angle
+            {
                 $.setup_block
 
-                # {$.function_name}(Num)
-                is_approx({$.function_name}(\$angle.num("radians")), \$desired_result, 
-                          "{$.function_name}(Num) - \{\$angle.num('radians')\} default");
-                for %official_base.keys -> \$base \{
-                    is_approx({$.function_name}(\$angle.num(\$base), %official_base\{\$base\}), \$desired_result, 
-                              "{$.function_name}(Num) - \{\$angle.num(\$base)\} \$base");
-                \}
+                # $.function_name(Num)
+                is_approx($.function_name($angle.num("radians")), $desired_result, 
+                          "$.function_name(Num) - {$angle.num('radians')} default");
+                for %official_base.keys -> $base {
+                    is_approx($.function_name($angle.num($base), %official_base{$base}), $desired_result, 
+                              "$.function_name(Num) - {$angle.num($base)} $base");
+                }
 
-                # Num.{$.function_name} tests
-                is_approx(\$angle.num("radians").{$.function_name}, \$desired_result, 
-                          "Num.{$.function_name} - \{\$angle.num('radians')\} default");
-                for %official_base.keys -> \$base \{
-                    is_approx(\$angle.num(\$base).{$.function_name}(%official_base\{\$base\}), \$desired_result, 
-                              "Num.{$.function_name} - \{\$angle.num(\$base)\} \$base");
-                \}
+                # Num.$.function_name tests
+                is_approx($angle.num("radians").$.function_name, $desired_result, 
+                          "Num.$.function_name - {$angle.num('radians')} default");
+                for %official_base.keys -> $base {
+                    is_approx($angle.num($base).$.function_name(%official_base{$base}), $desired_result, 
+                              "Num.$.function_name - {$angle.num($base)} $base");
+                }
 
-                # {$.function_name}(Rat)
-                is_approx({$.function_name}(\$angle.rat("radians")), \$desired_result, 
-                          "{$.function_name}(Rat) - \{\$angle.rat('radians')\} default");
-                for %official_base.keys -> \$base \{
-                    is_approx({$.function_name}(\$angle.rat(\$base), %official_base\{\$base\}), \$desired_result, 
-                              "{$.function_name}(Rat) - \{\$angle.rat(\$base)\} \$base");
-                \}
+                # $.function_name(Rat)
+                is_approx($.function_name($angle.rat("radians")), $desired_result, 
+                          "$.function_name(Rat) - {$angle.rat('radians')} default");
+                for %official_base.keys -> $base {
+                    is_approx($.function_name($angle.rat($base), %official_base{$base}), $desired_result, 
+                              "$.function_name(Rat) - {$angle.rat($base)} $base");
+                }
 
-                # Rat.{$.function_name} tests
-                is_approx(\$angle.rat("radians").{$.function_name}, \$desired_result, 
-                          "Rat.{$.function_name} - \{\$angle.rat('radians')\} default");
-                for %official_base.keys -> \$base \{
-                    is_approx(\$angle.rat(\$base).{$.function_name}(%official_base\{\$base\}), \$desired_result, 
-                              "Rat.{$.function_name} - \{\$angle.rat(\$base)\} \$base");
-                \}
+                # Rat.$.function_name tests
+                is_approx($angle.rat("radians").$.function_name, $desired_result, 
+                          "Rat.$.function_name - {$angle.rat('radians')} default");
+                for %official_base.keys -> $base {
+                    is_approx($angle.rat($base).$.function_name(%official_base{$base}), $desired_result, 
+                              "Rat.$.function_name - {$angle.rat($base)} $base");
+                }
 
-                # {$.function_name}(Int)
-                is_approx({$.function_name}(\$angle.int("degrees"), %official_base\{"degrees"\}), \$desired_result, 
-                          "{$.function_name}(Int) - \{\$angle.int('degrees')\} degrees");
-                is_approx(\$angle.int('degrees').{$.function_name}(%official_base\{'degrees'\}), \$desired_result, 
-                          "Int.{$.function_name} - \{\$angle.int('degrees')\} degrees");
+                # $.function_name(Int)
+                is_approx($.function_name($angle.int("degrees"), %official_base{"degrees"}), $desired_result, 
+                          "$.function_name(Int) - {$angle.int('degrees')} degrees");
+                is_approx($angle.int('degrees').$.function_name(%official_base{'degrees'}), $desired_result, 
+                          "Int.$.function_name - {$angle.int('degrees')} degrees");
 
                 # # Complex tests
-                # my Complex \$zp0 = \$angle.complex(0.0, "radians");
-                # my Complex \$sz0 = \$desired_result + 0i;
-                # my Complex \$zp1 = \$angle.complex(1.0, "radians");
-                # my Complex \$sz1 = (exp(\$zp1 * 1i) - exp(-\$zp1 * 1i)) / 2i;
-                # my Complex \$zp2 = \$angle.complex(2.0, "radians");
-                # my Complex \$sz2 = (exp(\$zp2 * 1i) - exp(-\$zp2 * 1i)) / 2i;
+                # my Complex $zp0 = $angle.complex(0.0, "radians");
+                # my Complex $sz0 = $desired_result + 0i;
+                # my Complex $zp1 = $angle.complex(1.0, "radians");
+                # my Complex $sz1 = (exp($zp1 * 1i) - exp(-$zp1 * 1i)) / 2i;
+                # my Complex $zp2 = $angle.complex(2.0, "radians");
+                # my Complex $sz2 = (exp($zp2 * 1i) - exp(-$zp2 * 1i)) / 2i;
                 # 
-                # # {$.function_name}(Complex) tests
-                # is_approx({$.function_name}(\$zp0), \$sz0, "{$.function_name}(Complex) - \$zp0 default");
-                # is_approx({$.function_name}(\$zp1), \$sz1, "{$.function_name}(Complex) - \$zp1 default");
-                # is_approx({$.function_name}(\$zp2), \$sz2, "{$.function_name}(Complex) - \$zp2 default");
+                # # $.function_name(Complex) tests
+                # is_approx($.function_name($zp0), $sz0, "$.function_name(Complex) - $zp0 default");
+                # is_approx($.function_name($zp1), $sz1, "$.function_name(Complex) - $zp1 default");
+                # is_approx($.function_name($zp2), $sz2, "$.function_name(Complex) - $zp2 default");
                 # 
-                # for %official_base.keys -> \$base \{
-                #     my Complex \$z = \$angle.complex(0.0, \$base);
-                #     is_approx({$.function_name}(\$z, %official_base\{\$base\}), \$sz0, "{$.function_name}(Complex) - \$z \$base");
+                # for %official_base.keys -> $base {
+                #     my Complex $z = $angle.complex(0.0, $base);
+                #     is_approx($.function_name($z, %official_base{$base}), $sz0, "$.function_name(Complex) - $z $base");
                 # 
-                #     \$z = \$angle.complex(1.0, \$base);
-                #     is_approx({$.function_name}(\$z, %official_base\{\$base\}), \$sz1, "{$.function_name}(Complex) - \$z \$base");
+                #     $z = $angle.complex(1.0, $base);
+                #     is_approx($.function_name($z, %official_base{$base}), $sz1, "$.function_name(Complex) - $z $base");
                 # 
-                #     \$z = \$angle.complex(2.0, \$base);
-                #     is_approx({$.function_name}(\$z, %official_base\{\$base\}), \$sz2, "{$.function_name}(Complex) - \$z \$base");
-                # \}
+                #     $z = $angle.complex(2.0, $base);
+                #     is_approx($.function_name($z, %official_base{$base}), $sz2, "$.function_name(Complex) - $z $base");
+                # }
                 # 
-                # # Complex.{$.function_name} tests
-                # is_approx(\$zp0.{$.function_name}, \$sz0, "Complex.{$.function_name} - \$zp0 default");
-                # is_approx(\$zp1.{$.function_name}, \$sz1, "Complex.{$.function_name} - \$zp1 default");
-                # is_approx(\$zp2.{$.function_name}, \$sz2, "Complex.{$.function_name} - \$zp2 default");
+                # # Complex.$.function_name tests
+                # is_approx($zp0.$.function_name, $sz0, "Complex.$.function_name - $zp0 default");
+                # is_approx($zp1.$.function_name, $sz1, "Complex.$.function_name - $zp1 default");
+                # is_approx($zp2.$.function_name, $sz2, "Complex.$.function_name - $zp2 default");
                 # 
-                # for %official_base.keys -> \$base \{
-                #     my Complex \$z = \$angle.complex(0.0, \$base);
-                #     #?rakudo skip "Complex.{$.function_name} plus base doesn't work yet"
-                #     is_approx(\$z.{$.function_name}(%official_base\{\$base\}), \$sz0, "Complex.{$.function_name} - \$z \$base");
+                # for %official_base.keys -> $base {
+                #     my Complex $z = $angle.complex(0.0, $base);
+                #     #?rakudo skip "Complex.$.function_name plus base doesn't work yet"
+                #     is_approx($z.$.function_name(%official_base{$base}), $sz0, "Complex.$.function_name - $z $base");
                 # 
-                #     \$z = \$angle.complex(1.0, \$base);
-                #     #?rakudo skip "Complex.{$.function_name} plus base doesn't work yet"
-                #     is_approx(\$z.{$.function_name}(%official_base\{\$base\}), \$sz1, "Complex.{$.function_name} - \$z \$base");
+                #     $z = $angle.complex(1.0, $base);
+                #     #?rakudo skip "Complex.$.function_name plus base doesn't work yet"
+                #     is_approx($z.$.function_name(%official_base{$base}), $sz1, "Complex.$.function_name - $z $base");
                 # 
-                #     \$z = \$angle.complex(2.0, \$base);
-                #     #?rakudo skip "Complex.{$.function_name} plus base doesn't work yet"
-                #     is_approx(\$z.{$.function_name}(%official_base\{\$base\}), \$sz2, "Complex.{$.function_name} - \$z \$base");
-                # \}
-            \}
+                #     $z = $angle.complex(2.0, $base);
+                #     #?rakudo skip "Complex.$.function_name plus base doesn't work yet"
+                #     is_approx($z.$.function_name(%official_base{$base}), $sz2, "Complex.$.function_name - $z $base");
+                # }
+            }
         ];
         $code.=subst: / ^^ ' ' ** 12 /, '', :g;
+        $code.=subst: '$.function_name', $.function_name, :g;
+        $code.=subst: '$.inverted_function_name', $.inverted_function_name, :g;
+        $code.=subst: '$.setup_block', $.setup_block, :g;
+        $code.=subst: '$.angle_and_results_name', $.angle_and_results_name, :g;
 
         $file.say: $code;
     }
     
     method dump_inverse_tests($file) {
-        my $code = qq[
-            # {$.inverted_function_name} tests
+        my $code = q[
+            # $.inverted_function_name tests
 
-            for $.angle_and_results_name -> \$angle
-            \{
+            for $.angle_and_results_name -> $angle
+            {
                 $.setup_block
 
-                # {$.function_name}(Num) tests
-                is_approx({$.function_name}({$.inverted_function_name}(\$desired_result)), \$desired_result, 
-                          "{$.inverted_function_name}(Num) - \{\$angle.num('radians')\} default");
-                for %official_base.keys -> \$base \{
-                    is_approx({$.function_name}({$.inverted_function_name}(\$desired_result, %official_base\{\$base\}), %official_base\{\$base\}), \$desired_result, 
-                              "{$.inverted_function_name}(Num) - \{\$angle.num(\$base)\} \$base");
-                \}
+                # $.function_name(Num) tests
+                is_approx($.function_name($.inverted_function_name($desired_result)), $desired_result, 
+                          "$.inverted_function_name(Num) - {$angle.num('radians')} default");
+                for %official_base.keys -> $base {
+                    is_approx($.function_name($.inverted_function_name($desired_result, %official_base{$base}), %official_base{$base}), $desired_result, 
+                              "$.inverted_function_name(Num) - {$angle.num($base)} $base");
+                }
                 
-                # Num.{$.function_name} tests
-                is_approx(\$desired_result.Num.{$.inverted_function_name}.{$.function_name}, \$desired_result, 
-                          "{$.inverted_function_name}(Num) - \{\$angle.num('radians')\} default");
-                for %official_base.keys -> \$base \{
-                    is_approx(\$desired_result.Num.{$.inverted_function_name}(%official_base\{\$base\}).{$.function_name}(%official_base\{\$base\}), \$desired_result,
-                              "{$.inverted_function_name}(Num) - \{\$angle.num(\$base)\} \$base");
-                \}
-            \}
+                # Num.$.function_name tests
+                is_approx($desired_result.Num.$.inverted_function_name.$.function_name, $desired_result, 
+                          "$.inverted_function_name(Num) - {$angle.num('radians')} default");
+                for %official_base.keys -> $base {
+                    is_approx($desired_result.Num.$.inverted_function_name(%official_base{$base}).$.function_name(%official_base{$base}), $desired_result,
+                              "$.inverted_function_name(Num) - {$angle.num($base)} $base");
+                }
+            }
         ];
         $code.=subst: / ^^ ' ' ** 12 /, '', :g;
+        $code.=subst: '$.function_name', $.function_name, :g;
+        $code.=subst: '$.inverted_function_name', $.inverted_function_name, :g;
+        $code.=subst: '$.setup_block', $.setup_block, :g;
+        $code.=subst: '$.angle_and_results_name', $.angle_and_results_name, :g;
+        
         $file.say: $code;
     }
 }
@@ -162,11 +170,11 @@ for $prelude.lines -> $line {
 }
 
 my $tf = TrigFunction.new("sin", "asin", "@sines", 
-                          "my \$desired_result = \$angle.result;");
+                          'my $desired_result = $angle.result;');
 $tf.dump_forward_tests($file);
 $tf.dump_inverse_tests($file);
 $tf = TrigFunction.new("cos", "acos", "@cosines", 
-                       "my \$desired_result = \$angle.result;");
+                       'my $desired_result = $angle.result;');
 $tf.dump_forward_tests($file);
 $tf.dump_inverse_tests($file);
 $tf = TrigFunction.new("tan", "atan", "@sines", 
@@ -190,11 +198,11 @@ $tf = TrigFunction.new("cotan", "acotan", "@sines",
 $tf.dump_forward_tests($file);
 $tf.dump_inverse_tests($file);
 
-$tf = TrigFunction.new("sinh", "asinh", "@sinhes", "my \$desired_result = \$angle.result;");
+$tf = TrigFunction.new("sinh", "asinh", "@sinhes", 'my $desired_result = $angle.result;');
 $tf.dump_forward_tests($file);
 $tf.dump_inverse_tests($file);
 $tf = TrigFunction.new("cosh", "acosh", "@coshes", 
-                       "my \$desired_result = \$angle.result;");
+                       'my $desired_result = $angle.result;');
 $tf.dump_forward_tests($file);
 $tf.dump_inverse_tests($file);
 $tf = TrigFunction.new("tanh", "atanh", "@sines", 
