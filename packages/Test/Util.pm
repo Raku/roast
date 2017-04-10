a40ba13554c5140cf11f3d3a4db444299fa9710d
Zoffix Znet <cpan@zoffix.com>
2017-04-10 12:17:46 -0400

    Implement fails-like
    Like throws-like + check that code fail()s instead of throw()ing

--- packages/Test/Util.pm
+++ packages/Test/Util.pm
@@ -264,6 +264,23 @@ sub make-temp-dir (Int $chmod? --> IO::Path:D) is export {
     p
 }
 
+sub fails-like (&test, $ex-type, $reason?, *%matcher) is export {
+    subtest $reason => sub {
+        plan 2;
+        CATCH { default {
+            with "expected code to fail but it threw {.^name} instead" {
+                .&flunk;
+                .&skip;
+                return False;
+            }
+        }}
+        my $res = test;
+        isa-ok $res, Failure, 'code returned a Failure';
+        throws-like { $res.sink }, $ex-type,
+            'Failure threw when sunk', |%matcher,
+    }
+}
+
 =begin pod
 
 =head1 NAME
@@ -464,6 +481,16 @@ Creates a semi-randomly named directory in C<$*TMPDIR>, optionally setting
 C<$chmod>, and returns an C<IO::Path> pointing to it. Automatically
 C<rmdir>s it with C<END> phaser. It's your responsibility to ensure the
 directory is empty at that time.
+
+=head2 fails-like(&test, $ex-type, $reason?, *%matcher)
+
+    fails-like(&test, $ex-type, $reason?, *%matcher)
+
+Like C<throws-like> but checks the code C<fail>s (as opposed to C<throw>ing).
+
+Executes C<&test> and uses C<Test.pm>'s C<isa-ok> to check the return value is a
+C<Failure>, then uses C<Test.pm>'s <throws-like> to check that C<Failure>
+throws the correct exception when sunk.
 
 =end pod
 
